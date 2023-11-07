a4735c5ad513f2223d488c9e7b4737ec7b8e68a8
cgen101 <chloe.gentry@gmail.com>
2023-11-07 14:46:06 -0600

    Implimented available book filtering. I need to figure out how to only display book title and id for filtered books

--- index.js
+++ index.js
@@ -3,17 +3,39 @@ var app = express();
 
 
 var books = [
-    {id: "1", title: "Reactions in REACT", author: "Ben Dover", publisher: "Random House", isbn: "978-3-16-148410-0", avail: true},
-    {id: "2", title: "Express-sions", author: "Freida Livery", publisher: "Chaotic House", isbn: "978-3-16-148410-2", avail: true},
-    {id: "3", title: "Restful REST", author: "Al Gorithm", publisher: "ACM", isbn: "978-3-16-143310-1", avail: true},
-    {id: "4", title: "See Essess", author: "Anna Log", publisher: "O'Reilly", isbn: "987-6-54-148220-1", avail: false, who: "Homer", due: "1/1/23"},
-    {id: "5", title: "Scripting in JS", author: "Dee Gital", publisher: "IEEE", isbn: "987-6-54-321123-1", avail: false, who: "Marge", due: "1/2/23"},
-    {id: "6", title: "Be An HTML Hero", author: "Jen Neric", publisher: "Coders-R-Us", isbn: "987-6-54-321123-2", avail: false, who: "Lisa", due: "1/3/23"}
+    {id: "1", title: "Reactions in REACT", author: "Ben Dover", publisher: "Random House", isbn: "978-3-16-148410-0", avail: "true"},
+    {id: "2", title: "Express-sions", author: "Freida Livery", publisher: "Chaotic House", isbn: "978-3-16-148410-2", avail: "true"},
+    {id: "3", title: "Restful REST", author: "Al Gorithm", publisher: "ACM", isbn: "978-3-16-143310-1", avail: "true"},
+    {id: "4", title: "See Essess", author: "Anna Log", publisher: "O'Reilly", isbn: "987-6-54-148220-1", avail: "false", who: "Homer", due: "1/1/23"},
+    {id: "5", title: "Scripting in JS", author: "Dee Gital", publisher: "IEEE", isbn: "987-6-54-321123-1", avail: "false", who: "Marge", due: "1/2/23"},
+    {id: "6", title: "Be An HTML Hero", author: "Jen Neric", publisher: "Coders-R-Us", isbn: "987-6-54-321123-2", avail: "false", who: "Lisa", due: "1/3/23"}
  ];
 
- app.get("/books", (req, res) => {
-    res.json(books);
+const booksMap = new Map();
+books.forEach(book => {
+    booksMap.set(book.id, { id: book.id, title: book.title });
+});
+
+app.get("/books", (req, res) => {
+   const { avail } = req.query;
+   const booksInfo = Array.from(booksMap.values());
+
+   if (avail === "true")
+   { 
+      const availBooks = books.filter((book) => book.avail === "true"); 
+      res.json(availBooks); 
+   } 
+   else if (avail === "false")
+   {
+      const unavailBooks = books.filter((book) => book.avail === "false"); 
+      res.json(unavailBooks); 
+   }
+   else
+   {
+    res.json(booksInfo);
     res.status(200); 
+    console.log("200 OK")
+   }
 });
 
 app.get("/books/:id", (req, res) => { 
@@ -24,10 +46,15 @@ app.get("/books/:id", (req, res) => {
    {
       res.json(book);
       res.status(200); 
+      console.log("200 BOO")
    }
    else 
+   {
       res.status(404); 
+      console.log("ERROR 404: book not found")
+   }
 });
+
 
 app.use(function(req, res, next) {   
    res.header('Access-Control-Allow-Origin', '*');   
